5c66eebd6dc2230cf4e762e852c85896b66a6d9b
MEC-97 <116013981+MEC-97@users.noreply.github.com>
2024-02-23 19:35:19 -0300

    3.3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const port = process.env.PORT;
 
- console.log(port)
+
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
